0a754ccc6a692507bc64d82596fbcfde486cf291
Varun Yadav <41319713+Viknight@users.noreply.github.com>
2025-08-25 07:03:39 +0100

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBtHg1-a20Tgn8DLtVQ_3wQDuzqWByVPVg",
-    authDomain: "employee-portal-cosmo-trace.firebaseapp.com",
-    projectId: "employee-portal-cosmo-trace",
-    storageBucket: "employee-portal-cosmo-trace.firebasestorage.app",
-    messagingSenderId: "283066633057",
-    appId: "1:283066633057:web:6eac992d6ad56c383ed320",
-    measurementId: "G-1C45C89CQH"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 }
 
 // Guard: throw helpful error if any value is missing
